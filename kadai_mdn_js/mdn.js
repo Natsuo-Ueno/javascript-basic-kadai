a54f2e5084dd947215ce1e84cd0839ef68eb5087
Natsuo-Ueno <b3cam972@gmail.com>
2025-10-05 23:48:44 +0900

    mdn.jsを修正
    "month"に1を加算して代入するよう修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,7 @@
 const year = new Date().getFullYear();
-const month = new Date().getMonth();
+
+const month = new Date().getMonth()+1;
+
 const day = new Date().getDate();
+
 console.log(year + '年' + month + '月' + day + '日');
