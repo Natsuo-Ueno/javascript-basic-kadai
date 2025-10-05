4563c59e503e2ed471075c6925abe7f554509a44
Natsuo-Ueno <b3cam972@gmail.com>
2025-10-05 18:40:46 +0900

    calc.jsを修正
    4～5行目の確認用のnum出力を削除

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,9 +1,6 @@
 // 変数numに1～100までのランダムな整数を代入する
 let num = Math.floor(Math.random()*100+1);
 
-// 変数numの値を出力する（確認用）
-console.log(num);
-
 //変数numが3と5の倍数の場合、「3と5の倍数です」という文字列を出力する
 if (num % 3 == 0 && num % 5 == 0) {
   console.log('3と5の倍数です');
